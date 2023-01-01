8ec3a335cfe6e4cb2ff2711030a1a09e8e6ce5be
marcussalome <marcus.salome@icarros.com.br>
2023-01-01 20:59:10 -0300

    changed file to typescript and applied lint rules

---
+++ constants.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  consts: {
+    COMMAND_SYMBOL: '!',
+    REJECT_CALLS: true,
+    LOADING_SCREEN: 'LOADING SCREEN',
+    QR: 'QR RECEIVED',
+    AUTHENTICATED: 'AUTHENTICATED',
+    AUTH_FAILURE: 'AUTHENTICATION FAILURE',
+    READY: 'READY',
+    DISCONNECTED: 'Client was logged out',
+    GROUP_JOIN: 'User joined.',
+    GROUP_LEAVE: 'User left.',
+    GROUP_UPDATE: 'Group updated',
+    CHANGE_STATE: 'CHANGE STATE',
+    MESSAGE_RECEIVED: 'MESSAGE RECEIVED',
+    DEFAULT_GROUP_TITLE: '🍞🛢️ Pão em Lata'
+  }
+}
