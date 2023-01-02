d305e94b26a9d25b72752aa38f0db15ceb543c5f
Marcus Salomé <marcuspedro7@gmail.com>
2023-01-01 21:29:06 -0300

    added simple type verification to function

--- actions/nomepadrao.ts
+++ actions/nomepadrao.ts
@@ -1,4 +1,4 @@
-export const  nomepadrao = async (msg, defaultGroupTitle) => {
+export const  nomepadrao = async (msg, defaultGroupTitle: String) => {
     let chat = await msg.getChat();
     if (chat.isGroup) {
         chat.setSubject(defaultGroupTitle);
