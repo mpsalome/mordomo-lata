30dc0a42e322684972bb23fc7425a23adacb7d7d
marcussalome <marcus.salome@icarros.com.br>
2023-01-05 01:22:50 -0300

    updated messageAck interface

--- interfaces/MessageAck.ts
+++ interfaces/MessageAck.ts
@@ -1,8 +1,8 @@
 export default interface MessageAck {
-  ACK_ERROR: string;
-  ACK_PENDING: string;
-  ACK_SERVER: string;
-  ACK_DEVICE: string;
-  ACK_READ: string;
-  ACK_PLAYED: string;
+  ACK_ERROR: string,
+  ACK_PENDING: string,
+  ACK_SERVER: string,
+  ACK_DEVICE: string,
+  ACK_READ: string,
+  ACK_PLAYED: string
 }
