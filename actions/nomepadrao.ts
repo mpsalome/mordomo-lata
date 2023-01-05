274530255b5193352b080f57382f596a90556c72
Marcus Salomé <marcuspedro7@gmail.com>
2023-01-05 16:24:14 -0300

    Changed to any type to prevent error

--- actions/nomepadrao.ts
+++ actions/nomepadrao.ts
@@ -1,10 +1,9 @@
 import { GroupChat } from 'whatsapp-web.js';
-import { Chat } from 'whatsapp-web.js';
 
 import { Message } from 'whatsapp-web.js'; 
 
 export const  nomepadrao = async (msg: Message, defaultGroupTitle: string) => {
-    let chat:GroupChat = await msg.getChat();
+    let chat:any = await msg.getChat();
     if (chat.isGroup) {
         chat.setSubject(defaultGroupTitle);
     }
