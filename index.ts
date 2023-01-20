29c6b645257903c4f48123bb832ce9de89fbe649
marcussalome <marcus.salome@icarros.com.br>
2023-01-20 09:55:15 -0300

    refactor: removed unused code

--- index.ts
+++ index.ts
@@ -62,44 +62,7 @@ client.on('disconnected', (reason: any) => {
   console.log(consts.DISCONNECTED, reason);
 });
 
-client.on('message_create', (msg: Message) => {
-  // Fired on all message creations, including your own
-  if (msg.fromMe) {
-    // do stuff here
-  }
-});
-
-client.on('message_revoke_everyone', async (after: any, before: any) => {
-  // Fired whenever a message is deleted by anyone (including you)
-  console.log(after); // message after it was deleted.
-  if (before) {
-    console.log(before); // message before it was deleted.
-  }
-});
-
-client.on('group_join', (notification: any) => {
-  // User has joined or been added to the group.
-  console.log('join', notification);
-  notification.reply(consts.GROUP_JOIN);
-});
-
-client.on('group_leave', (notification: any) => {
-  // User has left or been kicked from the group.
-  console.log('leave', notification);
-  notification.reply(consts.GROUP_LEAVE);
-});
-
-client.on('group_update', (notification: any) => {
-  // Group picture, subject or description has been updated.
-  console.log(consts.GROUP_UPDATE, notification);
-});
-
-client.on('change_state', (state: any) => {
-  console.log(consts.CHANGE_STATE, state);
-});
-
 client.on('message', async (msg: Message) => {
-  console.log(consts.MESSAGE_RECEIVED, msg);
   if (!msg.body.startsWith(`${consts.COMMAND_SYMBOL}`)) return;
 
   //used commands that have parameters
