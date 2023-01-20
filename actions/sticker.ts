58630fce105a06adbb213dbba8d07810d0d93853
Marcus Salomé <marcuspedro7@gmail.com>
2023-01-20 00:01:50 -0300

    feature: implements text on sticker creation

--- actions/sticker.ts
+++ actions/sticker.ts
@@ -1,14 +1,19 @@
 import { Message, MessageMedia } from 'whatsapp-web.js';
+import consts from '../constants';
+const Jimp = require("jimp");
+const path = require("path");
+const imagePath = path.resolve("./resources/images");
+const fontPath = path.resolve("./resources/fonts");
 
 export const sticker = async (msg: Message) => {
     let media: MessageMedia = new MessageMedia(``, ``, ``);
     if (msg.hasMedia) {
-        media = await msg.downloadMedia();
+        media = await getMessageMedia(msg);
     } else {
         if (msg.hasQuotedMsg) {
             let quotedMsg = await msg.getQuotedMessage();
             if (quotedMsg.hasMedia) {
-                media = await quotedMsg.downloadMedia();
+                media = await getMessageMedia(msg);
             } else {
                 return;
             }
@@ -23,3 +28,21 @@ export const sticker = async (msg: Message) => {
     msg.reply(media, chat.chatId, { sendMediaAsSticker: true });
     return;
 };
+
+async function getMessageMedia(msg: Message) {
+    let media: MessageMedia = await msg.downloadMedia();
+    if (msg.body.split(` `).length > 1) {
+        await textOverlay(media.data, msg.body.split(`${consts.COMMAND_SYMBOL}sticker`)[1]);
+        media = await MessageMedia.fromFilePath(`${imagePath}/sticker.png`);
+        return media;
+    }
+    return media
+}
+
+async function textOverlay(base64: string, text: string) {
+    const imageBuffer = Buffer.from(base64, 'base64');
+    const image = await Jimp.read(imageBuffer);
+    const font = await Jimp.loadFont(`${fontPath}/maximum_impact/GRJ3inO74NleyXBCDFftvUtn.ttf.fnt`);
+    image.print(font, 40, 272, `${text}`);
+    await image.writeAsync(`${imagePath}/sticker.png`);
+}
