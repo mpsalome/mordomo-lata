2b5295d9f6049478d28210d5a26e832b0287120c
Marcus Salomé <marcuspedro7@gmail.com>
2023-01-20 21:26:33 -0300

    feat: now salve command can answer w/ mentioned user

--- actions/salve.ts
+++ actions/salve.ts
@@ -1,12 +1,24 @@
-import { Message, MessageMedia } from "whatsapp-web.js";
+import { Contact, Message, MessageMedia } from "whatsapp-web.js";
 const Jimp = require("jimp");
 const path = require("path");
 const imagePath = path.resolve("./resources/images");
 const fontPath = path.resolve("./resources/fonts");
 
 export const salve = async (msg: Message) => {
+  const mentioned: Contact[] = await msg.getMentions();
   const chat: any = await msg.getChat();
   if (chat.isGroup) {
+    if (msg.body.split(` `).length > 1 && mentioned) {
+      await textOverlay(`${mentioned[0].pushname}`);
+      const stickerGeneral: MessageMedia = MessageMedia.fromFilePath(`${imagePath}/salve_sticker.png`)
+
+      msg.reply(stickerGeneral, chat.chatId, {
+        sendMediaAsSticker: true,
+      });
+  
+      return;
+    }
+
     if (msg.hasQuotedMsg) {
       const quotedMsg = await msg.getQuotedMessage();
       const contact = await quotedMsg.getContact();
