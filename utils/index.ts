4f0759154b1a07829ae9437eda45acd71e680f48
Marcus Salomé <marcuspedro7@gmail.com>
2024-04-30 01:40:22 -0300

    refactor: refact logger function
    removes chalk
    create log files

--- utils/index.ts
+++ utils/index.ts
@@ -2,6 +2,7 @@ import * as consts from './constants';
 import { ChatId, Client, ClientOptions, Contact, GroupParticipant, Message, MessageContent, MessageSendOptions } from "whatsapp-web.js";
 import { CommandDefinition } from '../interfaces/CommandDefinition';
 import winston from 'winston';
+const { combine, timestamp, printf, colorize, align } = winston.format;
 import chalk from 'chalk';
 
 export async function replyQuotedMsg(msg: Message, content: MessageContent, chatId?: ChatId | undefined, options?: MessageSendOptions | undefined) {
@@ -39,32 +40,28 @@ export function processCommand(msg:Message, actions:CommandDefinition<Message>[]
 }
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format((info) => {
-      const coloredLevel = getColorizedLevel(info.level);
-      info.level = coloredLevel;
-      return info;
-    })(),
-    winston.format.simple()
+  level: process.env.LOG_LEVEL || 'info',
+  format: combine(
+    colorize({ all: true }),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+    }),
+    align(),
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
   ),
-  transports: [
-    new winston.transports.Console()
-  ]
+  transports: 
+  [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'combined.log',
+    }),
+    new winston.transports.File({
+      filename: 'app-error.log',
+      level: 'error',
+    }),
+  ],
 });
 
-function getColorizedLevel(level: string): string {
-  switch (level) {
-    case 'info':
-      return chalk.bgBlueBright(`[${level.toUpperCase()}]`);
-    case 'warn':
-      return chalk.bgYellow(`[${level.toUpperCase()}]`);
-    case 'error':
-      return chalk.bgRed(`[${level.toUpperCase()}]`);
-    default:
-      return chalk.bgWhite(`[${level.toUpperCase()}]`);
-  }
-}
 
 export function log(message: string, level: 'info' | 'warn' | 'error' | 'default'): void {
   logger.log(level, message);
