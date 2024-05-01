58231b8504bb2bb2998acdee30d9ebdb0499250a
Marcus Salomé <marcuspedro7@gmail.com>
2024-05-01 12:50:09 -0300

    refactor: errorhandling on sticker command

--- actions/handlers/sticker.ts
+++ actions/handlers/sticker.ts
@@ -1,13 +1,20 @@
 import { Chat, Message, MessageMedia } from 'whatsapp-web.js';
-import { replyQuotedMsg } from '../../utils';
+import { replyQuotedMsg, handleError } from '../../utils';
 
-export default async (msg: Message) => {
-    const chat: Chat = await msg.getChat();
-    let media: MessageMedia = (msg.hasMedia && !msg.hasQuotedMsg) ? await msg.downloadMedia() : {} as MessageMedia
-    if (msg.hasQuotedMsg && (await msg.getQuotedMessage()).hasMedia) {
-        const quotedMsg:Message = await msg.getQuotedMessage();
-        media = await quotedMsg.downloadMedia();
+export default async function processMessage(msg: Message): Promise<void> {
+    try {
+        const chat: Chat = await msg.getChat();
+        let media: MessageMedia = {} as MessageMedia;
+
+        if (msg.hasMedia && !msg.hasQuotedMsg) {
+            media = await msg.downloadMedia();
+        } else if (msg.hasQuotedMsg && (await msg.getQuotedMessage()).hasMedia) {
+            const quotedMsg: Message = await msg.getQuotedMessage();
+            media = await quotedMsg.downloadMedia();
+        }
+
+        await replyQuotedMsg(msg, media, chat.id, { sendMediaAsSticker: true });
+    } catch (error) {
+        handleError(error);
     }
-    replyQuotedMsg(msg, media, chat.id, { sendMediaAsSticker: true })
-    return;
-};
+}
