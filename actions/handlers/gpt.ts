240cde8f79bd6a505c974d8de9532f5ad88c435b
Marcus Salomé <marcuspedro7@gmail.com>
2024-05-01 10:47:33 -0300

    refactor: include trycatch in gpt file

--- actions/handlers/gpt.ts
+++ actions/handlers/gpt.ts
@@ -1,21 +1,26 @@
 import { Message } from 'whatsapp-web.js';
-import * as consts from '../../utils/constants'
+import { COMMAND_SYMBOL } from '../../utils/constants';
 const { Configuration, OpenAIApi } = require("openai");
+import { log } from '../../utils/index';
 
 export default async (msg: Message) => {
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
-    });
-
-    const openai = new OpenAIApi(configuration);
-
-    const question:string = msg.body.split(`${consts.COMMAND_SYMBOL}meconta`)[1];
-    const answer:string = await askQuestion(question, openai);
-    msg.reply(`${answer}`);
+    try {
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+    
+        const openai = new OpenAIApi(configuration);
+    
+        const question:string = msg.body.split(`${COMMAND_SYMBOL}meconta`)[1];
+        const answer:string = await askQuestion(question, openai);
+        msg.reply(`${answer}`);
+    } catch (error) {
+        log(`Error: ${error.message}`, 'error');
+    }
     return;
 }
 
-async function askQuestion(question: string, openai: any): Promise<string> {
+async function askQuestion(question: string, openai: typeof OpenAIApi): Promise<string> {
     try {
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
@@ -24,7 +29,7 @@ async function askQuestion(question: string, openai: any): Promise<string> {
         });
         return (completion.data.choices[0].text.trim());
     } catch (error) {
-        return `Erro na API: ${error}`
+        log(`Error in OpenAI API: ${error.message}`, 'error');
+        throw error;
     }
-    
 }
