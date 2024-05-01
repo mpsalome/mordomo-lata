22f67eb42aa4331eac7276f6d38200ed09bc494a
Marcus Salomé <marcuspedro7@gmail.com>
2024-05-01 12:47:53 -0300

    refactor: refact 'salve' command

--- actions/handlers/salve.ts
+++ actions/handlers/salve.ts
@@ -1,56 +1,57 @@
-import { Contact, Message, MessageMedia } from "whatsapp-web.js";
+import { Chat, Message, MessageMedia, Contact } from "whatsapp-web.js";
+import { replyQuotedMsg, handleError } from "../../utils/index";
 const Jimp = require("jimp");
 const path = require("path");
+
 const imagePath = path.resolve("./resources/images");
 const fontPath = path.resolve("./resources/fonts");
 
-export default async (msg: Message) => {
-  const mentioned: Contact[] = await msg.getMentions();
-  const chat: any = await msg.getChat();
-  if (chat.isGroup) {
-    if (msg.body.split(` `).length > 1 && mentioned) {
-      await textOverlay(`${mentioned[0].pushname}`);
-      sendSticker(msg, chat);
-      return;
-    }
-
-    if (msg.hasQuotedMsg) {
-      const quotedMsg = await msg.getQuotedMessage();
-      const contact = await quotedMsg.getContact();
-      try {
-        await textOverlay(`${contact.pushname}`);
-        sendSticker(msg, chat);
-        return;
-
-      } catch (error) {
-        quotedMsg.reply(`Deu erro: ${error}, @${contact.id.user}`, chat.chatId, {
-          mentions: [contact]
-        });
-      }
-      return;
-    }
-
-    await textOverlay(`PESSOAL`);
-
-    sendSticker(msg, chat);
-    return;
+export default async function processMessage(msg: Message): Promise<void> {
+  try {
+    const chat: Chat = await msg.getChat();
+    if (!chat.isGroup) return;
+    const sticker: MessageMedia = await getSticker(msg);
+    await replyQuotedMsg(msg, sticker, chat.id, { sendMediaAsSticker: true });
+  } catch (error) {
+    handleError(error);
   }
-  return;
-};
-
-async function textOverlay(name: string) {
-  const image = await Jimp.read(`${imagePath}/salve_blank.png`);
-  const font = await Jimp.loadFont(`${fontPath}/bebas_neue/fn0Aw5cPObO5pPproIXmFG1e.ttf.fnt`);
-  image.print(font, 12, 375, `${name}`);
-  await image.writeAsync(`${imagePath}/salve_sticker.png`);
+}
+async function createStickerImageWithContact(name: string): Promise<string> {
+  try {
+    const image = await Jimp.read(`${imagePath}/salve_blank.png`);
+    const font = await Jimp.loadFont(`${fontPath}/bebas_neue/fn0Aw5cPObO5pPproIXmFG1e.ttf.fnt`);
+    image.print(font, 12, 375, `${name}`);
+    await image.writeAsync(`${imagePath}/salve_sticker.png`);
+    return `${imagePath}/salve_sticker.png`;
+  } catch (error) {
+    throw error;
+  }
 }
 
-function sendSticker(msg: Message, chat: any) {
-  const stickerGeneral: MessageMedia = MessageMedia.fromFilePath(`${imagePath}/salve_sticker.png`)
+async function getSticker(msg: Message): Promise<MessageMedia> {
+  try {
+    const name: string = await getContactName(msg);
+    const filePath: string = await createStickerImageWithContact(name);
+    return MessageMedia.fromFilePath(filePath);
+  } catch (error) {
+    throw error;
+  }
+}
 
-  msg.reply(stickerGeneral, chat.chatId, {
-    sendMediaAsSticker: true,
-  });
+async function getContactName(msg: Message): Promise<string> {
+  try {
+    const mentioned: Contact[] = await msg.getMentions();
+    
+    if (msg.body.split(" ").length > 1 && mentioned) return mentioned[0].pushname;
+    
+    if (msg.hasQuotedMsg) {
+      const quotedMsg: Message = await msg.getQuotedMessage();
+      const contact: Contact = await quotedMsg.getContact();
+      return contact.pushname;
+    }
 
-  return;
+    return "PESSOAL";
+  } catch (error) {
+    throw error;
+  }
 }
