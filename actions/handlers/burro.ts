5356a8da911ef8a6a98423c6ebe3644063c29481
Marcus Salomé <marcuspedro7@gmail.com>
2024-05-01 10:37:00 -0300

    refactor: include trycatch in "burro"

--- actions/handlers/burro.ts
+++ actions/handlers/burro.ts
@@ -1,14 +1,19 @@
 import { Message, MessageMedia, Chat } from 'whatsapp-web.js';
-import { replyQuotedMsg } from '../../utils/index'
+import { replyQuotedMsg, log } from '../../utils/index';
 const path = require("path");
+
 const audioPath = path.resolve("./resources/audios");
 const imagePath = path.resolve("./resources/images");
 
 export default async (msg: Message) => {
-  const donkey_image: MessageMedia = MessageMedia.fromFilePath(`${imagePath}/burro.jpeg`);
-  const donkey_audio: MessageMedia = MessageMedia.fromFilePath(`${audioPath}/burro.mpeg`);
-  const chat: Chat = await msg.getChat();
-  replyQuotedMsg(msg, donkey_image, chat.id, { sendMediaAsSticker: true })
-  replyQuotedMsg(msg, donkey_audio, chat.id)
-  return;
+  try {
+    const donkeyImage: MessageMedia = MessageMedia.fromFilePath(`${imagePath}/burro.jpeg`);
+    const donkeyAudio: MessageMedia = MessageMedia.fromFilePath(`${audioPath}/burro.mpeg`);
+    const chat: Chat = await msg.getChat();
+    
+    replyQuotedMsg(msg, donkeyImage, chat.id, { sendMediaAsSticker: true });
+    replyQuotedMsg(msg, donkeyAudio, chat.id);
+  } catch (error) {
+    log(`Error: ${error.message}`, 'error');
+  }
 };
