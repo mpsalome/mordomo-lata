6c8d104cdc0750d50d1d1a5296ba590d07578dda
Marcus Salomé <marcuspedro7@gmail.com>
2024-05-01 10:34:01 -0300

    refactor: refact 'all' command

--- actions/handlers/all.ts
+++ actions/handlers/all.ts
@@ -1,18 +1,25 @@
 import { Chat, Client, Contact, GroupChat, GroupParticipant, Message } from 'whatsapp-web.js';
-import { replyQuotedMsg, getContactsFrom } from '../../utils/index'
+import { replyQuotedMsg, getContactsFrom } from '../../utils/index';
+import { log } from '../../utils/';
 
 export default async (msg: Message, client: Client) => {
-  const chat: Chat = await msg.getChat()
-  if (chat.isGroup) {
-    const group: GroupChat = chat as GroupChat;
-    const participants: GroupParticipant[] = group.participants
-    const mentions: Contact[] = await getContactsFrom(participants, client)
-    let text = `🚨 *ATENÇÃO* 🚨\n`
-    mentions.forEach( contact => {
-      text+= `@${contact.id.user} `
-    })
-    replyQuotedMsg(msg, text.trim(), group.id, { mentions })
-    return;
+  try {
+    const chat: Chat = await msg.getChat();
+
+    if (chat.isGroup) {
+      const group: GroupChat = chat as GroupChat;
+      const participants: GroupParticipant[] = group.participants;
+      const mentions: Contact[] = await getContactsFrom(participants, client);
+
+      let text = `🚨 *ATENÇÃO* 🚨\n`;
+      mentions.forEach(contact => {
+        text += `@${contact.id.user} `;
+      });
+
+      log(`Sending message in group: ${group.id} - mentions ${ mentions }` , 'info');
+      await replyQuotedMsg(msg, text.trim(), group.id, { mentions });
+    }
+  } catch (error) {
+    log('Error processing message:', 'error');
   }
-  return;
 };
