1c85280f319d05d2db8ae3560ed4c05f8ff3569e
Marcus Salomé <marcuspedro7@gmail.com>
2024-05-01 11:48:05 -0300

    fix: migrate openai to new version

--- actions/handlers/gpt.ts
+++ actions/handlers/gpt.ts
@@ -1,35 +1,33 @@
 import { Message } from 'whatsapp-web.js';
 import { COMMAND_SYMBOL } from '../../utils/constants';
-const { Configuration, OpenAIApi } = require("openai");
-import { log } from '../../utils/index';
+import OpenAI from 'openai';
+import { handleError, log } from '../../utils/index';
 
 export default async (msg: Message) => {
     try {
-        const configuration = new Configuration({
-            apiKey: process.env.OPENAI_API_KEY,
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY
         });
-    
-        const openai = new OpenAIApi(configuration);
-    
         const question:string = msg.body.split(`${COMMAND_SYMBOL}meconta`)[1];
         const answer:string = await askQuestion(question, openai);
         msg.reply(`${answer}`);
     } catch (error) {
-        log(`Error: ${error.message}`, 'error');
+        handleError(error);
     }
     return;
 }
 
-async function askQuestion(question: string, openai: typeof OpenAIApi): Promise<string> {
+async function askQuestion(question: string, openai:OpenAI): Promise<string> {
     try {
-        const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${question}`,
+        const completion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo-0125",
+            messages:  [{"role": "user", "content": `${question}`}],
             "max_tokens": 1000
         });
-        return (completion.data.choices[0].text.trim());
+        log(JSON.stringify(completion))
+        return ((completion.choices[0].message.content ?? 'Algum erro com a API').toString().trim());
     } catch (error) {
-        log(`Error in OpenAI API: ${error.message}`, 'error');
+        handleError(error);
         throw error;
     }
 }
