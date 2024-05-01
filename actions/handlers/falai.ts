d53f94fbaacbb14a91f8b30f86d426ffe1c562e6
Marcus Salomé <marcuspedro7@gmail.com>
2024-05-01 10:43:19 -0300

    refactor: include trycatch in 'falai'

--- actions/handlers/falai.ts
+++ actions/handlers/falai.ts
@@ -1,33 +1,46 @@
 import { Message, MessageMedia } from 'whatsapp-web.js';
 import * as consts from '../../utils/constants';
 import * as googleTTS from 'google-tts-api';
+import { replyQuotedMsg, log } from '../../utils/index';
 
 export default async (msg: Message) => {
-    let audioUrl: string = ''
-
-    if (msg.hasQuotedMsg) {
-        const quotedMsg: Message = await msg.getQuotedMessage();
-        const text: string = quotedMsg.body;
-        audioUrl = await createAudioUrlFrom(text);
-    } else {
-        const text = msg.body.split(`${consts.COMMAND_SYMBOL}falai`)[1]
-        audioUrl = await createAudioUrlFrom(text);
+    try {
+        let audioUrl: string = '';
+        if (msg.hasQuotedMsg) {
+            const quotedMsg: Message = await msg.getQuotedMessage();
+            const text: string = quotedMsg.body;
+            audioUrl = await createAudioUrlFrom(text);
+        } else {
+            const text = msg.body.split(`${consts.COMMAND_SYMBOL}falai`)[1];
+            audioUrl = await createAudioUrlFrom(text);
+        }
+        await sendAudioMessage(msg, audioUrl);
+    } catch (error) {
+        log(`Error: ${error.message}`, 'error');
     }
-    await sendAudioMessage(msg, audioUrl);
-    return
-}
+};
 
 async function createAudioUrlFrom(text: string): Promise<string> {
-    const url = googleTTS.getAudioUrl(text, {
-        lang: 'pt_br',
-        slow: false,
-        host: 'https://translate.google.com',
-    });
-    return url;
+    try {
+        const url = googleTTS.getAudioUrl(text, {
+            lang: 'pt_br',
+            slow: false,
+            host: 'https://translate.google.com',
+        });
+        return url;
+    } catch (error) {
+        log(`Error creating audio URL: ${error.message}`, 'error');
+        throw error;
+    }
 }
 
 async function sendAudioMessage(msg: Message, audioUrl: string): Promise<void> {
-    const audioMedia: MessageMedia = await MessageMedia.fromUrl(audioUrl, { unsafeMime: true });
-    msg.reply(audioMedia);
+    try {
+        const audioMedia: MessageMedia = await MessageMedia.fromUrl(audioUrl, { unsafeMime: true });
+        msg.reply(audioMedia);
+    } catch (error) {
+        log(`Error sending audio message: ${error.message}`, 'error');
+        throw error;
+    }
     return;
 }
